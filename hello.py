2c68445491b89d66e860af0b55cdf32aff521f21
Shuaibbb <Shuaib212121@gmail.com>
2026-09-04 15:30:06 +0800

    Second post

--- hello.py
+++ hello.py
@@ -2,3 +2,20 @@ print("===================================")
 print("Welcome here")
 print("My first post!")
 print("===================================")
+
+username = "Shuaib21"
+bio = "I am a software developer and I love coding."
+followers = 150
+
+followers += 50  
+print("Day 1", followers)
+
+followers += 20  
+print("Day 2", followers)
+
+followers -= 10  
+print("Day 3", followers)
+
+print ("Username:", username)
+print ("Bio:", bio)
+print ("Followers:", followers)
