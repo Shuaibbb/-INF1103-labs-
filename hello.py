a71a0779fd848dc9534080f8435f8555a8d27a74
Shuaibbb <Shuaib212121@gmail.com>
2026-09-04 15:41:13 +0800

    Third post

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
+#Activity 1
 print("===================================")
 print("Welcome here")
 print("My first post!")
 print("===================================")
 
+#Activity 2 
 username = "Shuaib21"
 bio = "I am a software developer and I love coding."
 followers = 150
 
+print ("Username:", username)
+print ("Bio:", bio)
+print ("Followers:", followers)
+
+#Activity 3
 followers += 50  
 print("Day 1", followers)
 
@@ -15,7 +22,3 @@ print("Day 2", followers)
 
 followers -= 10  
 print("Day 3", followers)
-
-print ("Username:", username)
-print ("Bio:", bio)
-print ("Followers:", followers)
