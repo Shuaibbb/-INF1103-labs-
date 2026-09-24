178410c9e9c100ad77a59c89b49648bcc7c527ae
Shuaibbb <Shuaib212121@gmail.com>
2026-09-24 20:57:22 +0800

    Refactor input and delivery processing into functions

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,22 +1,37 @@
+def get_valid_input():
+    stock = input("Enter stock quantity or type quit: ")
+
+    if stock == "quit":
+        return "quit"
+    elif stock.isdigit():
+        return int(stock)
+    else:
+        print("Invalid stock quantity")
+        return None
+
+
+def process_delivery(current_total, new_value):
+    new_total = current_total + new_value
+    return new_total
+
+
 inventory = 0
 failed_entries = 0
 
 while True:
-    stock = input("Enter stock quantity or type quit: ")
+    stock = get_valid_input()
 
     if stock == "quit":
         break
-    elif stock.isdigit():
-        quantity = int(stock)
-        inventory = inventory + quantity
+    elif stock is None:
+        failed_entries = failed_entries + 1
+    else:
+        inventory = process_delivery(inventory, stock)
         print("Current inventory:", inventory)
 
         if inventory > 500:
             print("Overstock alert")
             break
-    else:
-        print("Invalid stock quantity")
-        failed_entries = failed_entries + 1
 
 print("Total Units Processed:", inventory)
 print("Number of Failed/Rejected Entries:", failed_entries)
