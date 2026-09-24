7b1250fcf6a6f2a61a8e6b99eee1d7635978be7b
Shuaibbb <Shuaib212121@gmail.com>
2026-09-24 20:59:54 +0800

    Add tax calculation and modular reporting

--- modular_auditor.py
+++ modular_auditor.py
@@ -15,8 +15,19 @@ def process_delivery(current_total, new_value):
     return new_total
 
 
+def calculate_tax(amount):
+    tax = amount * 0.10
+    return tax
+
+
+def generate_report(total_units, failed_attempts):
+    print("Total Units Processed:", total_units)
+    print("Number of Failed/Rejected Entries:", failed_attempts)
+
+
 inventory = 0
 failed_entries = 0
+deliveries_processed = 0
 
 while True:
     stock = get_valid_input()
@@ -27,11 +38,15 @@ while True:
         failed_entries = failed_entries + 1
     else:
         inventory = process_delivery(inventory, stock)
+        deliveries_processed = deliveries_processed + 1
+        tax = calculate_tax(stock)
+
         print("Current inventory:", inventory)
+        print("Tax for this delivery:", tax)
 
         if inventory > 500:
             print("Overstock alert")
             break
 
-print("Total Units Processed:", inventory)
-print("Number of Failed/Rejected Entries:", failed_entries)
+print("Total Deliveries Processed:", deliveries_processed)
+generate_report(inventory, failed_entries)
